d9e51fcf76beb7b91fd28bf7489dcbb424880c1c
lisotunali <lisotunali@hotmail.com>
2020-05-11 15:24:02 +0200

    Contact class is made with an unique ID for each Contact and with all Get and Set methods. (+ quick fix for unique ID)

--- src/Contact.java
+++ src/Contact.java
@@ -4,16 +4,12 @@ public class Contact {
     private String phoneNumber;
     private String email;
     private String address;
-    private Long contactID;
-    private static Long uniqueID = 0L;
 
     public Contact(String name, String phoneNumber, String email, String address){
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.address = address;
-        this.uniqueID++;
-        contactID = getUniqueID();
     }
 
     public String getName(){
@@ -30,10 +26,6 @@ public class Contact {
 
     public String getAddress() {
         return address;
-    }
-
-    public Long getUniqueID(){
-        return uniqueID;
     }
 
     public void setName (String name){
