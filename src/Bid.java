ca9e158016ff2ad98a56b1f1950fbd4dd636ad61
Brian Spit <brian@mythenatic.com>
2020-05-11 15:41:57 +0200

    Create bid class

--- src/Bid.java
+++ src/Bid.java
@@ -1,2 +1,17 @@
 public class Bid {
+    private Integer price;
+    private Person person;
+
+    public Bid(Integer price, Person person) {
+        this.price = price;
+        this.person = person;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
 }
