1012d91ab68744eca190f64d78ca1c346c6ccfa3
Brian Spit <brian@mythenatic.com>
2020-05-11 13:33:14 +0200

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Person {
+    private String name;
+    private String password;
+    private ArrayList<Contact> addressBook;
+    private ArrayList<Animal> hasLivestock;
+
+    public Person(String name, String password) {
+        this.name = name;
+        this.password = password;
+
+        //Todo:
+        // Add addressbook
+        // Add livestock
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addAnimal(Animal animal) {
+    }
+
+    public Animal getAnimal(LivestockType type) {
+    }
+
+    public void getAnimals() {
+    }
+
+    public ArrayList<Contact> getAddressBook() {
+    }
+
+    public void addConcact(Contact contact) {
+    }
+}
