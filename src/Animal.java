9d4b954fdd694205200593ad206137be5a017a87
Gyan Ramdjas <gyanramdjas@hotmail.com>
2020-05-11 16:17:57 +0200

    Animal en animalproduct gemaakt

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,23 @@
 public class Animal {
+    private Integer amountInStock;
+    private LivestockType type;
+
+    public Animal (LivestockType type, Integer amountInStock){
+        this.type = type ;
+        this.amountInStock = amountInStock;
+    }
+
+    public Integer getAmount(){
+        return amountInStock;
+    }
+
+    public LivestockType getType() {
+        return type;
+    }
+
+    public void setAmount(Integer amount){
+        amountInStock = amount;
+    }
+
 }
+
