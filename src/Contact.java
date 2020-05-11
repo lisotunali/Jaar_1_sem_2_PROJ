c2240b21c254a76b96e64f0c84259a5d3491de45
lisotunali <lisotunali@hotmail.com>
2020-05-11 15:13:21 +0200

    Contact class is made with an unique ID for each Contact and with all Get and Set methods.

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,50 @@
 public class Contact {
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private static Long contactID = 0L;
+
+    public Contact(String name, String phoneNumber, String email, String address){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.contactID++;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail (String email){
+        this.email = email;
+    }
+
+    public void setAddress (String address){
+        this.address = address;
+    }
+
+
 }
