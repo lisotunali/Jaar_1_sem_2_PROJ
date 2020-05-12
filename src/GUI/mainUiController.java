cd554de667ef807e7977a47b09d5a8c044ba4c8b
Brian Spit <brian@mythenatic.com>
2020-05-12 17:18:11 +0200

    Add on click event for profile

--- src/GUI/mainUiController.java
+++ src/GUI/mainUiController.java
@@ -1,4 +1,9 @@
 package GUI;
 
+import java.io.IOException;
+
 public class mainUiController {
+    public void profileButtonClicked() throws IOException {
+        Main.switchSceneTo("profile");
+    }
 }
