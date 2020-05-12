113bdec26fbc0a5df123bc90ded32c6b10322a3f
Brian Spit <brian@mythenatic.com>
2020-05-12 17:17:23 +0200

    Create profile

---
+++ src/GUI/profileController.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import BACKEND.Person;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class profileController {
+    public TextField nameInput;
+    public PasswordField passwordInput;
+    public PasswordField passwordConfirmInput;
+    public Label errorLabel;
+
+    // Initialize with current user data
+    public void initialize() {
+        Person current = singletonPerson.getInstance();
+        nameInput.setText(current.getName());
+        passwordInput.setText(current.getPassword());
+    }
+
+    // Updates the user in the database with the new values
+    public void onUpdateClicked() {
+        if (passwordInput.getText().equals(passwordConfirmInput.getText())) {
+            fakeDatabase.updateUserPassword(singletonPerson.getInstance(), passwordInput.getText());
+            System.out.println("Updated password");
+        } else {
+            errorLabel.setText("Error updating profile password is not the same");
+        }
+    }
+
+    public void onBackClicked() throws IOException {
+        Main.switchSceneTo("mainUi");
+    }
+}
