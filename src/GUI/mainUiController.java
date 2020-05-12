c7908b6b61bc1c5bdcfec3f221dafefb76c07817
lisotunali <lisotunali@hotmail.com>
2020-05-12 18:40:25 +0200

    Contacts is finished! (for the first time)

--- src/GUI/mainUiController.java
+++ src/GUI/mainUiController.java
@@ -1,4 +1,10 @@
 package GUI;
 
+import java.io.IOException;
+
 public class mainUiController {
+
+    public void contactsButtonClicked() throws IOException {
+        Main.switchSceneTo("contacts");
+    }
 }
