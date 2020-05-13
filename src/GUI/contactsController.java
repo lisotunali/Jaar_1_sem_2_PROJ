df91dac6b2fc693c59efee461ac0028d6d2813d1
lisotunali <lisotunali@hotmail.com>
2020-05-13 13:42:25 +0200

    Contacts no longer throws nullpointers when buttons are pressed

--- src/GUI/contactsController.java
+++ src/GUI/contactsController.java
@@ -69,11 +69,13 @@ public class contactsController {
 
     public void contactsRemoveButtonClicked() throws IOException {
         Contact contactToBeDeleted = tableView.getSelectionModel().getSelectedItem();
-        for (Contact contact : singletonPerson.getInstance().getAddressBook()) {
-            if (contact.getName().equals(contactToBeDeleted.getName())) {
-                singletonPerson.getInstance().removeContact(contact);
-                Main.switchSceneTo("contacts");
-                return;
+        if (contactToBeDeleted != null) {
+            for (Contact contact : singletonPerson.getInstance().getAddressBook()) {
+                if (contact.getName().equals(contactToBeDeleted.getName())) {
+                    singletonPerson.getInstance().removeContact(contact);
+                    Main.switchSceneTo("contacts");
+                    return;
+                }
             }
         }
 
@@ -81,10 +83,12 @@ public class contactsController {
 
     public void contactsEditButtonClicked() throws IOException {
         Contact contact = tableView.getSelectionModel().getSelectedItem();
-        contact.setName(nameTextField.getText());
-        contact.setPhoneNumber(phoneTextField.getText());
-        contact.setEmail(emailTextField.getText());
-        contact.setAddress(addressTextField.getText());
-        Main.switchSceneTo("contacts");
+        if (contact != null) {
+            contact.setName(nameTextField.getText());
+            contact.setPhoneNumber(phoneTextField.getText());
+            contact.setEmail(emailTextField.getText());
+            contact.setAddress(addressTextField.getText());
+            Main.switchSceneTo("contacts");
+        }
     }
 }
