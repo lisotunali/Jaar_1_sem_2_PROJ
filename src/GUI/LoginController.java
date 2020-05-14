f309f47ea235c1bd29a1524d95e5dd0305803402
Brian Spit <brian@mythenatic.com>
2020-05-14 19:16:21 +0200

    Added error alert and do nothing when fields are empty

--- src/GUI/LoginController.java
+++ src/GUI/LoginController.java
@@ -1,12 +1,12 @@
 package GUI;
 
 import BACKEND.Person;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class LoginController {
     public TextField nameInput;
@@ -15,25 +15,16 @@ public class LoginController {
     public Button registerButton;
 
     public void loginButtonClicked() throws IOException {
-        ArrayList<Person> users = fakeDatabase.getUserDatabase();
+        if (nameInput.getText().isEmpty() || passwordInput.getText().isEmpty()) return;
 
         for (Person person : fakeDatabase.getUserDatabase()) {
             if (person.getName().equals(nameInput.getText()) && person.getPassword().equals(passwordInput.getText())) {
                 singletonPerson.setPerson(person);
-
-//                Product product = new Product(25, "Test Product", "Dit is een testproduct voor de marketplace", 999, 10, person);
-//                marketplace.addProduct(product);
-//                if (marketplace.getAllProducts().isEmpty()) {
-//                    System.out.println("Marketplace is empty!");
-//                }
-//                if (!(marketplace.getAllProducts().isEmpty())) {
-//                    System.out.println("Marketplace contains products");
-//                }
                 Main.switchSceneTo("mainUi");
             }
         }
 
-        // TODO: Add error message.
+        Main.showAlert(Alert.AlertType.ERROR, "Invalid credentials");
     }
 
     public void registerButtonClicked() throws IOException {
