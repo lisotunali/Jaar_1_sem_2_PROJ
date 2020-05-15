be809af537afeda3b6875293c4d3a756aa891b9d
RuggeDX <rishwan@live.nl>
2020-05-15 22:54:06 +0200

    Everything is done.

--- src/GUI/MarketplaceController.java
+++ src/GUI/MarketplaceController.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import BACKEND.Bid;
+import BACKEND.Person;
 import BACKEND.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -42,13 +43,15 @@ public class MarketplaceController {
     public void bidButtonAction() {
         System.out.println("Trying to place bid...");
         //If no product is selected
-        if (productTable.getSelectionModel().getSelectedItem() == null) {
+        Product selectedItem = productTable.getSelectionModel().getSelectedItem();
+        if (selectedItem == null) {
             AlertClass.showAlert(Alert.AlertType.WARNING, "Please select a product before trying to bid.");
             return;
         }
 
         //If seller tries to bid on their own product
-        if (singletonPerson.getInstance().equals(productTable.getSelectionModel().getSelectedItem().getPerson())) {
+        Person currentUser = singletonPerson.getInstance();
+        if (currentUser.equals(selectedItem.getPerson())) {
             AlertClass.showAlert(Alert.AlertType.ERROR, "You can't place a bid on your own product.");
             return;
         }
@@ -61,19 +64,19 @@ public class MarketplaceController {
             return;
         }
         //If bid is lower than current highest price
-        if (!(singletonPerson.getInstance().equals(productTable.getSelectionModel().getSelectedItem().getPerson())) && bidInput <= productTable.getSelectionModel().getSelectedItem().getPrice()) {
+        if (!(currentUser.equals(selectedItem.getPerson())) && bidInput <= selectedItem.getPrice()) {
             System.out.println("Bid is lower than current highest.");
             AlertClass.showAlert(Alert.AlertType.ERROR, "Your bid is not higher than current highest bid.");
             return;
         }
 
         //If bid is valid
-        if (!(singletonPerson.getInstance().equals(productTable.getSelectionModel().getSelectedItem().getPerson())) && bidInput > productTable.getSelectionModel().getSelectedItem().getPrice()) {
-            productTable.getSelectionModel().getSelectedItem().setPrice(bidInput);
+        if (!(currentUser.equals(selectedItem.getPerson())) && bidInput > selectedItem.getPrice()) {
+            selectedItem.setPrice(bidInput);
 
-            productTable.getSelectionModel().getSelectedItem().addBid(new Bid(bidInput, singletonPerson.getInstance(), productTable.getSelectionModel().getSelectedItem().getPerson(), singletonPerson.getInstance().getName(), productTable.getSelectionModel().getSelectedItem().getAdvertTitle()));
-            System.out.println("Price: €" + bidInput + " " + "Bid from: " + singletonPerson.getInstance().getName());
-            System.out.println(productTable.getSelectionModel().getSelectedItem().getBids());
+            selectedItem.addBid(new Bid(bidInput, currentUser, selectedItem.getPerson(), currentUser.getName(), selectedItem.getAdvertTitle()));
+            System.out.println("Price: €" + bidInput + " " + "Bid from: " + currentUser.getName());
+            System.out.println(selectedItem.getBids());
 
             System.out.println("Bid placed");
             productTable.refresh();
