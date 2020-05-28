2ffe44141a8ebdae644100e1ee177f2f88617e01
noahelmenyari <noah.menyari@gmail.com>
2020-05-28 12:57:35 +0200

    Writing screen created in SceneBuilder

--- src/Education/writingController.java
+++ src/Education/writingController.java
@@ -1,5 +1,6 @@
 package Education;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
@@ -7,5 +8,7 @@ public class writingController {
 
     private ImageView imageView;
     private TextField guessField;
+    private Button guessButton;
+
 
 }
