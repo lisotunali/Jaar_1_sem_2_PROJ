dc5732d7ecb7578229c65ec5ac753dd1daa489b1
noahelmenyari <noah.menyari@gmail.com>
2020-05-29 19:11:05 +0200

    Added amount of letters on screen for writing game

--- src/Education/writingController.java
+++ src/Education/writingController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,9 +29,15 @@ public class writingController extends GameController{
     @FXML
     private Label timerLabel;
 
+    @FXML
+    private Text letterText;
+
+    private ArrayList<ImageWithName> questions = new ArrayList<>();
+
     public void initialize() throws IOException {
         game = new Game();
         game.saveQuestionsLocally();
+        questions = game.getCurrentGameQuestions();
         shuffleQuestions();
         nextQuestion();
         startTimer(timerLabel);
@@ -38,24 +45,30 @@ public class writingController extends GameController{
 
     public void nextQuestion() throws IOException {
         game.setCurrentQuestion(game.getCurrentQuestion()+1);
-        if (game.getCurrentGameQuestions().size() -1 == game.getCurrentQuestion()){
+        if (questions.size() -1 == game.getCurrentQuestion()){
             endGame(game);
             System.out.println("Game ended");
             return;
         }
         else {
-            imageView.setImage(game.getCurrentGameQuestions().get(game.getCurrentQuestion()).getImage());
+            imageView.setImage(questions.get(game.getCurrentQuestion()).getImage());
+            StringBuilder dashes = new StringBuilder();
+            for(int i = 0; i < questions.get(game.getCurrentQuestion()).getName().length(); i++){
+                dashes.append("_ ");
+            }
+            guessField.setPromptText(questions.get(game.getCurrentQuestion()).getName().length() + " letters");
+            letterText.setText(dashes + "    " + questions.get(game.getCurrentQuestion()).getName().length() + " letters");
             System.out.println("image set - index" + game.getCurrentQuestion());
         }
 
     }
 
     public void shuffleQuestions(){
-        Collections.shuffle(game.getCurrentGameQuestions());
+        Collections.shuffle(questions);
     }
 
     public void doneClicked() throws IOException {
-        checkAnswer(guessField.getText(), game.getCurrentGameQuestions().get(game.getCurrentQuestion()).getName());
+        checkAnswer(guessField.getText(), questions.get(game.getCurrentQuestion()).getName());
         guessField.clear();
         nextQuestion();
     }
