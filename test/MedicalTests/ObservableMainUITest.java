e55c1274d23bcc5e913965833c5655b9d6955928
noahelmenyari <noah.menyari@gmail.com>
2020-06-12 17:30:57 +0200

    Doctor-specific profile functions tested

--- test/MedicalTests/ObservableMainUITest.java
+++ test/MedicalTests/ObservableMainUITest.java
@@ -20,12 +20,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ObservableMainUITest extends TestFXTestBase {
 
-    @Test
+    @Test //De gebruiker kijkt naar zijn afspraken op het hoofdscherm. De lijst met afspraken wordt gewijzigd (er wordt een nieuwe afspraak gemaakt hier). Het hoofdscherm van de gebruiker moet door de verandering zichzelf updaten.
     public void updateObservableScheduleTest() throws IOException {
         singletonPerson.setPerson(new Person("test instance", "123"));
         SceneController.switchTo("mainUI");
         fakeDatabase.getUserDatabase().add(new Doctor("Teendokter", "123", SpecializationType.GENERAL));
-        assertNotNull(SingletonAppointments.getInstance().createAppointment(LocalDate.of(2020, 6, 23), "Teenvocht onsteking", SpecializationType.GENERAL, singletonPerson.getInstance()), "Afspraak niet aangemaakt.");
+        assertNotNull(SingletonAppointments.getInstance().createAppointment(LocalDate.of(2020, 6, 23), "Teenvocht onsteking", SpecializationType.GENERAL, singletonPerson.getInstance()), "Afspraak niet aangemaakt. Test zal falen.");
         System.out.println(SingletonAppointments.getInstance().getAllAppointments(singletonPerson.getInstance()));
         clickOn("#userAppointments");
         try {
